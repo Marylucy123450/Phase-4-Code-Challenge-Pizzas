57d88f772a1cf3c94bef2bfa878ae21c73342ff8
Marylucy123450 <marylucyatienoomenda@gmail.com>
2023-03-19 20:13:32 +0300

    editing

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -2,10 +2,15 @@ class RestaurantPizzasController < ApplicationController
   def create
     @restaurant_pizza = RestaurantPizza.new(restaurant_pizza_params)
     if @restaurant_pizza.save
-      render json: @restaurant_pizza.pizza.as_json(only: [:id, :name, :ingredients])
+      render json: @restaurant_pizza.pizza.as_json(only: [:id, :name, :ingredients]), status: :created
     else
       render json: { errors: @restaurant_pizza.errors.full_messages }, status: :unprocessable_entity
     end
+  end
+
+  def index
+    @restaurant_pizzas = RestaurantPizza.all
+    render json: @restaurant_pizzas.as_json(include: { pizza: { only: [:id, :name, :ingredients] } })
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-pizza1 = Pizza.create(name: "Chicken", ingredients: "Dough, Tomato Sauce, Chicken")
-pizza2 = Pizza.create(name: "Pepper", ingredients: "Dough, Tomato Sauce, Cheese, Pepper")
-pizza3 = Pizza.create(name: "Vegetable", ingredients: "Dough, Tomato Sauce, Cheese, Mushrooms, Peppers, Onions")
+r1 = Restaurant.create(name: 'Sottocasa NYC', address: '298 Atlantic Ave, Brooklyn, NY 11201')
+r2 = Restaurant.create(name: 'PizzArte', address: '69 W 55th St, New York, NY 10019')
 
-restaurant1 = Restaurant.create(name: "Lolwe", address: "44, Nairobi, N 3273")
-restaurant2 = Restaurant.create(name: "Vivy", address: "69 , Kisumu, K 3333")
+p1 = Pizza.create(name: 'Cheese', ingredients: 'Dough, Tomato Sauce, Cheese')
+p2 = Pizza.create(name: 'Pepperoni', ingredients: 'Dough, Tomato Sauce, Cheese, Pepperoni')
 
-
+r1.restaurant_pizzas.create(price: 10, pizza: p1)
+r1.restaurant_pizzas.create(price: 12, pizza: p2)
+r2.restaurant_pizzas.create(price: 15, pizza: p1)
