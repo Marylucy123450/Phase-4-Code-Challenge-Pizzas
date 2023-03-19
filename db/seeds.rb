60869e2076dd65683ae69a7dfd89097a33c8471b
Marylucy123450 <marylucyatienoomenda@gmail.com>
2023-03-19 12:29:12 +0300

    seed.rb changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-pizza1 = Pizza.create(name: "Cheese", ingredients: "Dough, Tomato Sauce, Cheese")
-pizza2 = Pizza.create(name: "Pepperoni", ingredients: "Dough, Tomato Sauce, Cheese, Pepperoni")
-pizza3 = Pizza.create(name: "Veggie", ingredients: "Dough, Tomato Sauce, Cheese, Mushrooms, Peppers, Onions")
+pizza1 = Pizza.create(name: "Chicken", ingredients: "Dough, Tomato Sauce, Chicken")
+pizza2 = Pizza.create(name: "Pepper", ingredients: "Dough, Tomato Sauce, Cheese, Pepper")
+pizza3 = Pizza.create(name: "Vegetable", ingredients: "Dough, Tomato Sauce, Cheese, Mushrooms, Peppers, Onions")
 
-restaurant1 = Restaurant.create(name: "Sottocasa NYC", address: "298 Atlantic Ave, Brooklyn, NY 11201")
-restaurant2 = Restaurant.create(name: "PizzArte", address: "69 W 55th St, New York, NY 10019")
-restaurant3
+restaurant1 = Restaurant.create(name: "Lolwe", address: "44, Nairobi, N 3273")
+restaurant2 = Restaurant.create(name: "Vivy", address: "69 , Kisumu, K 3333")
+
 
